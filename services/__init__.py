36b9ed981fa2dae6b63e80daa71e1e60f84f1f18
Bruno Filipe <bmfilipe@gmail.com>
2026-07-31 15:41:31 +0100

    refactor: replace runpy with exec for app execution and initialize services and ui packages

--- services/__init__.py
+++ services/__init__.py
@@ -1,12 +1,4 @@
 try:
-    from services.gemini_ingestion import GeminiIngestionService
-    from services.odds_api_ingestion import OddsApiService
-    from services.oddsportal_ingestion import OddsPortalIngestionService
-    from services.api_football_ingestion import ApiFootballIngestionService
-    from services.exporter import ReportExporter
-    from services.email_service import EmailService
-    from services.colab_generator import ColabNotebookGenerator
-except ImportError:
     from .gemini_ingestion import GeminiIngestionService
     from .odds_api_ingestion import OddsApiService
     from .oddsportal_ingestion import OddsPortalIngestionService
@@ -14,7 +6,24 @@ except ImportError:
     from .exporter import ReportExporter
     from .email_service import EmailService
     from .colab_generator import ColabNotebookGenerator
+except (ImportError, KeyError, Exception):
+    from services.gemini_ingestion import GeminiIngestionService
+    from services.odds_api_ingestion import OddsApiService
+    from services.oddsportal_ingestion import OddsPortalIngestionService
+    from services.api_football_ingestion import ApiFootballIngestionService
+    from services.exporter import ReportExporter
+    from services.email_service import EmailService
+    from services.colab_generator import ColabNotebookGenerator
 
-__all__ = ["GeminiIngestionService", "OddsApiService", "OddsPortalIngestionService", "ApiFootballIngestionService", "ReportExporter", "EmailService", "ColabNotebookGenerator"]
+__all__ = [
+    "GeminiIngestionService",
+    "OddsApiService",
+    "OddsPortalIngestionService",
+    "ApiFootballIngestionService",
+    "ReportExporter",
+    "EmailService",
+    "ColabNotebookGenerator"
+]
+
 
 
--- ui/__init__.py
+++ ui/__init__.py
@@ -1,15 +1,15 @@
 try:
-    from ui.styles import apply_custom_styles
-    from ui.tab_ingestion import render_tab_ingestion
-    from ui.tab_analysis import render_tab_analysis
-    from ui.tab_slips import render_tab_slips
-    from ui.tab_admin import render_tab_admin
-except ImportError:
     from .styles import apply_custom_styles
     from .tab_ingestion import render_tab_ingestion
     from .tab_analysis import render_tab_analysis
     from .tab_slips import render_tab_slips
     from .tab_admin import render_tab_admin
+except (ImportError, KeyError, Exception):
+    from ui.styles import apply_custom_styles
+    from ui.tab_ingestion import render_tab_ingestion
+    from ui.tab_analysis import render_tab_analysis
+    from ui.tab_slips import render_tab_slips
+    from ui.tab_admin import render_tab_admin
 
 __all__ = [
     "apply_custom_styles",
@@ -18,4 +18,5 @@ __all__ = [
     "render_tab_slips",
     "render_tab_admin"
 ]
+
 
